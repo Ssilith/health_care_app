1fcc29e07c03a8a2b04aa7056ede1c4fffb1e517
Katarzyna-Hajduk <72875387+Ssilith@users.noreply.github.com>
2025-05-01 14:57:08 +0200

    web rss

--- test/unit_tests/utils/process_info_stub.dart
+++ test/unit_tests/utils/process_info_stub.dart
@@ -1,1 +1,11 @@
-int getRssKb() => -1;
+// ignore: deprecated_member_use, avoid_web_libraries_in_flutter
+import 'dart:html';
+
+int getRssKb() {
+  try {
+    final mem = (window.performance as dynamic).memory;
+    return ((mem.usedJSHeapSize as num) ~/ 1024);
+  } catch (_) {
+    return -1;
+  }
+}
