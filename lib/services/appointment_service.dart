a1a00f0731346e8501b03ce2266887e2bc8c2f8e
Katarzyna-Hajduk <72875387+Ssilith@users.noreply.github.com>
2025-05-02 13:45:42 +0200

    fix

--- lib/services/appointment_service.dart
+++ lib/services/appointment_service.dart
@@ -1,9 +1,11 @@
 import 'package:health_care_app/model/appointment.dart';
 import 'package:health_care_app/services/repository.dart';
+import 'package:health_care_app/services/repository_impl.dart';
 
 class AppointmentService {
   final Repository _repository;
 
+  AppointmentService() : _repository = RepositoryImpl();
   AppointmentService.withRepository(this._repository);
 
   Future<List<Appointment>> getAllAppointments() async {
