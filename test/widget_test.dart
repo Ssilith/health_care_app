3ae4fbbf618d413957ca9ef4f78cfcd19843ff10
Katarzyna-Hajduk <72875387+Ssilith@users.noreply.github.com>
2025-05-05 15:57:50 +0200

    New golden tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,6 +12,13 @@ import 'widget_tests/popup_window_buttons_test.dart' as popup_btn;
 import 'widget_tests/search_bar_filter_test.dart' as search_bar;
 import 'widget_tests/chat_bubble_golden_test.dart' as chat_bubble_gold;
 import 'widget_tests/action_container_golden_test.dart' as action_gold;
+import 'widget_tests/text_input_form_golden_test.dart' as input_gold;
+import 'widget_tests/search_bar_container_golden_test.dart' as search_gold;
+import 'widget_tests/rectangular_button_golden_test.dart' as rect_gold;
+import 'widget_tests/popup_window_golden_test.dart' as popup_gold;
+import 'widget_tests/main_switch_golden_test.dart' as switch_gold;
+import 'widget_tests/blank_scaffold_golden_test.dart' as scaffold_gold;
+import 'widget_tests/appointment_container_golden_test.dart' as appt_gold;
 
 import 'utils/benchmark_helper.dart';
 import 'utils/firebase_test_setup.dart';
@@ -32,6 +39,13 @@ void main() {
     search_bar.main();
     chat_bubble_gold.main();
     action_gold.main();
+    input_gold.main();
+    search_gold.main();
+    rect_gold.main();
+    popup_gold.main();
+    switch_gold.main();
+    scaffold_gold.main();
+    appt_gold.main();
   });
 
   tearDownAll(dumpPerfReports);
