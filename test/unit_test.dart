d0748665100abe65d605f63ec3fabbc3334d9101
Katarzyna-Hajduk <72875387+Ssilith@users.noreply.github.com>
2025-05-06 14:39:09 +0200

    Clear unit tests

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -5,7 +5,6 @@ import 'unit_tests/chat_service_test.dart' as chat_tests;
 import 'unit_tests/chat_service_error_test.dart' as chat_err_tests;
 import 'unit_tests/geo_service_test.dart' as geo_tests;
 import 'unit_tests/geo_service_empty_test.dart' as geo_empty_tests;
-import 'unit_tests/insert_pdf_service_test.dart' as insert_pdf_tests;
 import 'unit_tests/appointment_service_test.dart' as appointment_tests;
 import 'unit_tests/ice_service_test.dart' as ice_tests;
 import 'unit_tests/notebook_service_test.dart' as notebook_tests;
@@ -18,7 +17,6 @@ void main() {
     chat_err_tests.main();
     geo_tests.main();
     geo_empty_tests.main();
-    insert_pdf_tests.main();
     appointment_tests.main();
     ice_tests.main();
     notebook_tests.main();
